fb4d5a556279fa3d3701412e809dd3ac8fc2b6ca
Larson Collier <collierlarson@gmail.com>
2023-03-30 14:26:34 -0400

    first commit

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([])
+
+  const request = async()=>{
+    let req = await fetch(`http://localhost:6001/plants`)
+    let res = await req.json()
+    console.log(res)
+  }
+
+  useEffect(()=>{
+    request()
+
+  },[])
+
   return (
     <main>
       <NewPlantForm />
